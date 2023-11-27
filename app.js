d6a80ab3c3c08b2f4f01b596e006803c9026c692
0laolu <abdullateefolaolu@gmail.com>
2023-11-27 10:17:26 +0100

    fix: improve ux by opening accordion when user clicks any element

--- app.js
+++ app.js
@@ -15,9 +15,21 @@ const setupGuideAccordion = document.querySelector(".setup-guide-list-wrapper")
 console.log(setupGuideAccordion)
 
 setupGuideAccordion.addEventListener("click", function(event) {
+    console.log(event.target)
+
+    // improving ux by displaying dropdown when any element is clicked
+
     if(event.target.className == "setup-guide-label") {
         event.target.parentElement.classList.toggle("active")
-    } else if (event.target.className == "dropdown-icon") {
+    } else if(event.target.className == "content-description" || event.target.className == "description-header") {
         event.target.parentElement.parentElement.parentElement.classList.toggle("active")
+    } else if (event.target.className == "dropdown-icon" || event.target.className == "form-progress") {
+        event.target.parentElement.parentElement.parentElement.classList.toggle("active")
+    } else if (event.target.className == "progress-count" || event.target.className == "progress-indicator") {
+        event.target.parentElement.parentElement.parentElement.parentElement.classList.toggle("active")
+    } else if (event.target.className == "progress-bar-container") {
+        event.target.parentElement.parentElement.parentElement.parentElement.parentElement.classList.toggle("active")
+    } else if (event.target.className == "progress-bar") {
+        event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.classList.toggle("active")
     }
 })
