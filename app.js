0066a1cc5d79889967a9730a360989607d4e9b13
0laolu <abdullateefolaolu@gmail.com>
2023-11-27 09:38:39 +0100

    feat: build setup guide accordion and add basic functionality

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ removeBtn.addEventListener("click", function() {
 })
 
 
+// loading and removing the content for set up guide
+
+const setupGuideAccordion = document.querySelector(".setup-guide-list-wrapper")
+console.log(setupGuideAccordion)
+
+setupGuideAccordion.addEventListener("click", function(event) {
+    if(event.target.className == "setup-guide-label") {
+        event.target.parentElement.classList.toggle("active")
+    } else if (event.target.className == "dropdown-icon") {
+        event.target.parentElement.parentElement.parentElement.classList.toggle("active")
+    }
+})
